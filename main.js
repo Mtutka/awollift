bd2dc72d92a8d137aeca247a2e99c00fefac2160
Mateusz Tutka <tutkamateusz1@gmail.com>
2020-06-07 17:40:26 +0200

    add burger slider

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const burger = document.querySelector(".burger");
+const burger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("show");
-  burger.classList.toggle("show");
+  burger.classList.toggle("is-active");
 });
