4045c9bb83c0a60ee108f647b79a4820c33ac935
Mateusz Tutka <tutkamateusz1@gmail.com>
2020-06-08 20:40:55 +0200

    add desktop wiew

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("show");
   burger.classList.toggle("is-active");
 });
+
+let prevScrollpos = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+  let width = window.innerWidth;
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos && width >= 1024) {
+    document.querySelector("nav").style.top = "0";
+  } else if (width >= 1024) {
+    document.querySelector("nav").style.top = "-7vh";
+  }
+  prevScrollpos = currentScrollPos;
+});
